0c4d902663cb9fae092d2d499bcdcf1f1c1a9b1d
Ian Watt <ianwatt@gmail.com>
2014-08-25 13:03:34 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -36,3 +36,14 @@ finally:
     
     if con:
         con.close() 
+        
+
+with con:    
+    
+    cur = con.cursor()    
+    cur.execute("SELECT * FROM Cars")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print row
