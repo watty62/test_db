b09413bc705886e92d40a9d98264b72c241d3f9b
Ian Watt <ianwatt@gmail.com>
2014-08-25 13:08:37 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,16 +10,16 @@ try:
     cur = con.cursor()  
 
     cur.executescript("""
-        DROP TABLE IF EXISTS Cars;
-        CREATE TABLE Cars(Id INT, Name TEXT, Price INT);
-        INSERT INTO Cars VALUES(1,'Audi',52642);
-        INSERT INTO Cars VALUES(2,'Mercedes',57127);
-        INSERT INTO Cars VALUES(3,'Skoda',9000);
-        INSERT INTO Cars VALUES(4,'Volvo',29000);
-        INSERT INTO Cars VALUES(5,'Bentley',350000);
-        INSERT INTO Cars VALUES(6,'Citroen',21000);
-        INSERT INTO Cars VALUES(7,'Hummer',41400);
-        INSERT INTO Cars VALUES(8,'Volkswagen',21600);
+        DROP TABLE IF EXISTS data;
+        CREATE TABLE data (Id INT, Name TEXT, Price INT);
+        INSERT INTO data VALUES(1,'Audi',52642);
+        INSERT INTO data VALUES(2,'Mercedes',57127);
+        INSERT INTO data VALUES(3,'Skoda',9000);
+        INSERT INTO data VALUES(4,'Volvo',29000);
+        INSERT INTO data VALUES(5,'Bentley',350000);
+        INSERT INTO data VALUES(6,'Citroen',21000);
+        INSERT INTO data VALUES(7,'Hummer',41400);
+        INSERT INTO data VALUES(8,'Volkswagen',21600);
         """)
 
     con.commit()
@@ -41,7 +41,7 @@ con = lite.connect('test.db')
 with con:    
     
     cur = con.cursor()    
-    cur.execute("SELECT * FROM Cars")
+    cur.execute("SELECT * FROM data")
 
     rows = cur.fetchall()
 
